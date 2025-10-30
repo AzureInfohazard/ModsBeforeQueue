d74e7ffc3926c5b00fc4cc6b98579093ea4741f8
AzureInfohazard <tavriebold@gmail.com>
2025-10-30 08:59:33 -0500

    Project Files

---
+++ ModsBeforeQueue/ModsBeforeQueue/ModsBeforeQueueModSystem.cs
@@ -0,0 +1,57 @@
+﻿using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Config;
+using Vintagestory.API.Server;
+using HarmonyLib;
+using Vintagestory.Server;
+using System.Reflection;
+using System;
+namespace ModsBeforeQueue
+{
+    [HarmonyPatch]
+    public class ModsBeforeQueueModSystem : ModSystem
+    {
+        private HarmonyLib.Harmony? _harmony;
+        private ICoreServerAPI _api;
+        public override bool ShouldLoad(EnumAppSide forSide) => forSide == EnumAppSide.Server;
+        // Called on server and client
+        // Useful for registering block/entity classes on both sides
+        public override void Start(ICoreAPI api)
+        {
+
+        }
+        public override void StartServerSide(ICoreServerAPI api)
+        {
+            _api = api;
+            _harmony = new Harmony(Mod.Info.ModID);
+            _harmony.PatchAll();
+
+        }
+       
+        public override void StartClientSide(ICoreClientAPI api)
+        {
+
+        }
+    }
+    [HarmonyPatch(typeof(ServerMain), "PreFinalizePlayerIdentification")]
+    public static class ModBeforeQueuePatch
+    {
+        public static bool Prefix(ServerMain __instance, Packet_ClientIdentification packet, ConnectedClient client, string entitlements)
+        {
+            MethodInfo createPackedid = AccessTools.Method(typeof(ServerMain), "CreatePacketIdentification", new Type[] { typeof(bool) });
+            if (createPackedid != null)
+            {
+                if (__instance.Clients.Count - 1 >= __instance.Config.MaxClients)
+                {
+                    if (__instance.Config.MaxClientsInQueue > 0)
+                    {
+                        __instance.SendPacket(client.Id, (Packet_Server)createPackedid.Invoke(__instance, new object[] { false }));
+                    }
+                }
+
+            }
+            return true;
+        }
+    }
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # ModsBeforeQueue
+##*THIS IS A SERVER SIDE MOD*
+##Allows Clients to download mods before being put into queue.
+Sends a extra server information packet before sending the normal server queue packet
+Currently looks to work with no known issues
+
+### How and What
+Currently adds a Harmony Prefix to PreFinalizePlayerIdentification checks to see if the server is full (to determine if client will be sent to queue) then sends a ServerIdentity Packet calling CreatePacketIdentification(false) to generate the packet with the HasPrivilege("controlserver") flag set to false, This is the only possible bug I can forsee if the client dosent recieve the second ServerInformation packet AND is a a server member with the controlserver ability, it might not ?display? correctly.
+
+ 
+
+*Thats it, Thats All*
