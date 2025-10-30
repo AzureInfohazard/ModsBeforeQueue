f56cf0c68dd69ecfea04803e33cf9f4815d69666
AzureInfohazard <tavriebold@gmail.com>
2025-10-30 16:37:59 -0500

    Modified to use Queueapi

---
+++ ModsBeforeQueue/ModsBeforeQueue/ModBeforeQueueJoinHandlerName_thing.cs
@@ -0,0 +1,32 @@
+﻿using Vintagestory.API.Server;
+using HarmonyLib;
+using Vintagestory.Server;
+using System.Reflection;
+using QueueAPI;
+using QueueAPI.Handlers;
+namespace ModsBeforeQueue
+{
+    public class ModBeforeQueueJoinHandlerName_thing(ICoreServerAPI api) : SimpleJoinQueueHandler(api)
+    {
+        private readonly ICoreServerAPI api = api;
+        private MethodInfo _createPacketIdentificationMethod = AccessTools.Method(typeof(ServerMain), "CreatePacketIdentification",[ typeof(bool) ]);
+
+        public override IJoinQueueHandler.IPlayerConnectResult OnPlayerConnect(Packet_ClientIdentification clientIdentPacket, ConnectedClient client, string entitlements)
+        {
+            var server = api.GetInternalServer();
+            lock (server.ConnectionQueue)
+            {
+
+                if (JoinedPlayerCount >= server.Config.MaxClients)
+                {
+                    var packet = (Packet_Server)_createPacketIdentificationMethod.Invoke(server, [false]);
+                    server.SendPacket(client.Id, packet);
+                }
+
+            }
+            
+            return base.OnPlayerConnect(clientIdentPacket, client, entitlements);
+        }
+    }
+}
+
--- ModsBeforeQueue/ModsBeforeQueue/ModsBeforeQueueModSystem.cs
+++ ModsBeforeQueue/ModsBeforeQueue/ModsBeforeQueueModSystem.cs
@@ -2,56 +2,21 @@
 using Vintagestory.API.Common;
 using Vintagestory.API.Config;
 using Vintagestory.API.Server;
-using HarmonyLib;
-using Vintagestory.Server;
-using System.Reflection;
 using System;
+using QueueAPI;
+using System.Numerics;
+using System.Net.Sockets;
 namespace ModsBeforeQueue
 {
-    [HarmonyPatch]
     public class ModsBeforeQueueModSystem : ModSystem
     {
-        private HarmonyLib.Harmony? _harmony;
-        private ICoreServerAPI _api;
         public override bool ShouldLoad(EnumAppSide forSide) => forSide == EnumAppSide.Server;
-        // Called on server and client
-        // Useful for registering block/entity classes on both sides
-        public override void Start(ICoreAPI api)
-        {
 
-        }
         public override void StartServerSide(ICoreServerAPI api)
         {
-            _api = api;
-            _harmony = new Harmony(Mod.Info.ModID);
-            _harmony.PatchAll();
-
+            api.ModLoader.GetModSystem<QueueAPIModSystem>().Handler = new ModBeforeQueueJoinHandlerName_thing(api);
         }
-       
-        public override void StartClientSide(ICoreClientAPI api)
-        {
-
-        }
-    }
-    [HarmonyPatch(typeof(ServerMain), "PreFinalizePlayerIdentification")]
-    public static class ModBeforeQueuePatch
-    {
-        public static bool Prefix(ServerMain __instance, Packet_ClientIdentification packet, ConnectedClient client, string entitlements)
-        {
-            MethodInfo createPackedid = AccessTools.Method(typeof(ServerMain), "CreatePacketIdentification", new Type[] { typeof(bool) });
-            if (createPackedid != null)
-            {
-                if (__instance.Clients.Count - 1 >= __instance.Config.MaxClients)
-                {
-                    if (__instance.Config.MaxClientsInQueue > 0)
-                    {
-                        __instance.SendPacket(client.Id, (Packet_Server)createPackedid.Invoke(__instance, new object[] { false }));
-                    }
-                }
-
-            }
-            return true;
-        }
+   
     }
 }
 
